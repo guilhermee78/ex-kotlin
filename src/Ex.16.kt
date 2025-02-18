8942c1c86e5425ccd0ecfcf5b02d7f877fc18b1c
Victor Henrique Souza Oliveira <2401280@salas.aulas>
2025-02-17 21:49:55 -0300

    Desafio

--- src/Ex.16.kt
+++
@@ -1,8 +0,0 @@
-fun main(){
-    print("Quantos cigarros você fuma por dia?  ")
-    var cigarro = readln().toInt()
-    print("Quantos anos você fuma?  ")
-    var anos = readln().toInt()
-    var fumados = cigarro
-
-}
